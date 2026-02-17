e5a797c5318c14874f67b0a7e3e282989b5e503d
newx - Isaac Turner <156273295+newx-it@users.noreply.github.com>
2026-02-17 11:54:45 -0700

    Update fingermap.h

--- config/fingermap.h
+++ config/fingermap.h
@@ -74,7 +74,7 @@ behaviors {
 		amt: a_mod_tap 				{ compatible = "zmk,behavior-hold-tap"; #binding-cells = <2>; flavor = "tap-preferred"; 
 										tapping-term-ms = <150>; bindings = <&kp>, <&kp>; display-name = "a-mod-Tap"; 				};
 		bmt: b_mod_tap 				{ compatible = "zmk,behavior-hold-tap"; #binding-cells = <2>; flavor = "tap-preferred"; 
-										tapping-term-ms = <300>; bindings = <&kp>, <&kp>; display-name = "b-mod-Tap"; 				};
+										tapping-term-ms = <225>; bindings = <&kp>, <&kp>; display-name = "b-mod-Tap"; 				};
 		XXleftmt: XXleft_mod_tap 	{ compatible = "zmk,behavior-hold-tap";  #binding-cells = <2>; flavor = "tap-preferred"; 
 										tapping-term-ms = <150>; bindings = <&XXleft>, <&kp>; display-name = "XXleft mod-Tap"; 		};
 		Xspacemt: Xspace_mod_tap 	{ compatible = "zmk,behavior-hold-tap"; #binding-cells = <2>; flavor = "tap-preferred"; 
